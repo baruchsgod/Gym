56ca8b6ecb63b5bd21780e04edb5a5ec83d8a5bf
maiky.rodriguez <baruchsgod@hotmail.com>
2021-04-10 16:43:35 -0600

    Pending fixing activity bug

---
+++ Gym/Models/Metric.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gym.Models
+{
+    public class Metric
+    {
+        public int Id { get; set; }
+
+        
+    }
+}
